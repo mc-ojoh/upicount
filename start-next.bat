c1607d076dd46b7576a216ce045435e660b2d6dd
お嬢 <ckb609@gmail.com>
2025-08-16 17:46:06 +0900

    aaaa

---
+++ start-next.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\temp\my-app
+echo Starting Next.js Docker container...
+docker compose up
+pause
---
+++ stop-next.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\temp\my-app
+echo Stopping Next.js Docker container...
+docker compose down
+pause
